84ff724ca9f5dd802e38d495bd564501124a4e6a
Maybach <mickael.doranges@gmail.com>
2023-05-23 15:45:28 -0400

    chore(): add  null-rc

--- after/plugin/null-rc.rc.lua
+++ after/plugin/null-rc.rc.lua
@@ -1,34 +1,45 @@
-local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
-
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
+local null_ls = require("null-ls")
+local sources = {
+	null_ls.builtins.formatting.prettier.with({
+		filetypes = {
+			"css",
+			"graphql",
+			"html",
+			"javascript",
+			"javascriptreact",
+			"json",
+			"less",
+			"markdown",
+			"scss",
+			"typescript",
+			"typescriptreact",
+			"yaml",
+		},
+		check_package_json = true,
+		runtime_condition = function(params)
+			-- return false to skip running prettier
+			return true
+		end,
+		timeout = 5000,
+	}),
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.code_actions.gitsigns,
+}
 null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
+	sources = sources,
+	on_attach = function(client)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.cmd([[
+					augroup LspFormatting
+							autocmd! * <buffer>
+							autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
+					augroup END
+				]])
+		end
+	end,
 })
-
+require("mason-null-ls").setup({
+	ensure_installed = nil,
+	automatic_installation = true,
+	automatic_setup = false,
+})
